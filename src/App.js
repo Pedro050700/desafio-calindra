eacdac76e7f93f983ae857befc5c5f4ab2bb42d6
Pedro050700 <61997113+Pedro050700@users.noreply.github.com>
2021-07-21 03:30:30 -0300

    Versão Final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import "./App.css";
 import SearchInput from "./SearchInput";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { Button, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 const api = "https://mystique-v2-americanas.juno.b2w.io/";
 
@@ -8,6 +10,7 @@ function App() {
   const [text, setText] = useState("");
   const [product, setProduct] = useState([]);
   const [suggestion, setSuggestion] = useState([]);
+  const [exibir, setExibir] = useState(false);
 
   function handlesubmite(event) {
     event.preventDefault();
@@ -15,6 +18,8 @@ function App() {
   }
 
   async function requisicaoApi() {
+    setExibir(true);
+
     fetch(
       `https://mystique-v2-americanas.juno.b2w.io/autocomplete?content=${text}&source=nanook`
     )
@@ -28,29 +33,53 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <h1>Busque aqui o seu produto</h1>
-        <form onSubmit={handlesubmite}>
-          <div>
-            <SearchInput value={text} onChange={setText}></SearchInput>
-            <input type="submit" />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>Busque aqui o seu produto</h1>
+            <form onSubmit={handlesubmite}>
+              <div>
+                <SearchInput value={text} onChange={setText}></SearchInput>
+                <Button className="button" variant="secondary" type="submit">
+                  Buscar
+                </Button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-      <div>
-        <div>
-          <h4>Produtos</h4>
-          {product &&
-            product.map((value) => <div key={value.id}>{value.name}</div>)}
         </div>
-
-        <div>
-          <h4>Sugestões</h4>
-          {suggestion &&
-            suggestion.map((value, indice) => (
-              <div key={indice}>{value.term}</div>
-            ))}
-        </div>
+        {exibir == true ? (
+          <div>
+            {" "}
+            <div className="row produtosList">
+              <h5>Produtos</h5>
+              {product &&
+                product.map((value) => (
+                  <Card className="col-12 col-md-4 ProdutoCard" key={value.id}>
+                    <Card.Body>
+                      <Card.Title>{value.name}</Card.Title>
+                      <Card.Text>{value.name} com o melhor preço!!</Card.Text>
+                      <a href="#">Saber mais</a>
+                    </Card.Body>
+                  </Card>
+                ))}
+            </div>
+            <div className="row sugestoes ">
+              <h5>Sugestões</h5>
+              {suggestion &&
+                suggestion.map((value, indice) => (
+                  <Card className="col-12 col-md-4 SugestaoCard" key={indice}>
+                    <Card.Body>
+                      <Card.Title>{value.term}</Card.Title>
+                      <Card.Text>{value.term} com o melhor preço!!</Card.Text>
+                      <a href="#">Saber mais</a>
+                    </Card.Body>
+                  </Card>
+                ))}
+            </div>
+          </div>
+        ) : (
+          <div></div>
+        )}
       </div>
     </div>
   );
--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,10 +1,20 @@
 import react, { useState } from "react";
+import { FormLabel, FormControl, Button } from "react-bootstrap";
 
 const SearchInput = ({ value, onChange }) => {
   function handleChange(event) {
     onChange(event.target.value);
   }
-  return <input type="search" value={value} onChange={handleChange} />;
+  return (
+    <FormLabel className="search" value={value} onChange={handleChange}>
+      <FormControl
+        placeholder="busque o seu produto"
+        aria-label="busque o seu produto"
+        aria-describedby="basic-addon2"
+      />
+    
+    </FormLabel>
+  );
 };
 
 export default SearchInput;
